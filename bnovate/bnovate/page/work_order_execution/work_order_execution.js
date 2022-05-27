f3499f70c59627c3b1060c299c2769afce09c037
Douglas Watson <douglas.watson@bnovate.com>
2022-05-27 18:00:09 +0200

    WOE: handle serial numbers, rough implementation

--- bnovate/bnovate/page/work_order_execution/work_order_execution.js
+++ bnovate/bnovate/page/work_order_execution/work_order_execution.js
@@ -16,16 +16,18 @@ The workflow goes:
 	- Submit -> Creates stock entry, redirects to "view" state
 
 
-The main view switches between two tables: read -> write (to adjust quantities, enter S/N....)
+The main view switches between two tables: read and write (to adjust quantities, enter S/N....)
+
+For serialized items, we force entry of components for one item at a time, so the "Submit" button becomes a "Next" button.
 */
 
-// TODO: what if same info is submitted / sent twice? <-- disable submit button. Can still have another tab open.
-// More visual feedback when STE is submitted.
-// Avoid double clicks on Finish?
-// Handle batches
+// TODO: what if same info is submitted / sent twice? <-- From another tab open for example.
 // Handle SNs.
-// TODO: auto-populate expiry date based on shelf-life when creating batch.
-
+// Handle expiry dates for fills and refills.
+// Fix valuation - client side scripts are no longer called. Maybe fetch my own custom script?
+//  Or move my script to a JS library function.
+// Consider removing "view" mode for serialized items.
+// Consider switching to serialized view if any serialized items are present. It shouldn't happen, but why not.
 
 frappe.provide("frappe.bnovate.work_order_execution")
 
@@ -42,11 +44,13 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 	const write = Symbol("write");
 
 	const state = {
-		work_order_id: null, 	// docname of the current workorder
-		work_order_doc: null,  	// contents of the current workorder
-		docinfo: null, 			// docinfo[doctype][docname] -> {attachments: []}
-		view: read,				// state of the items display: read or write
-		ste_doc: null, 			// will contain content of stock entry before submitting.
+		work_order_id: null, 		// docname of the current workorder
+		work_order_doc: null,  		// contents of the current workorder
+		docinfo: null, 				// docinfo[doctype][docname] -> {attachments: []}
+		view: read,					// state of the items display: read or write
+		ste_doc: null, 				// will contain content of stock entry before submitting.
+		produce_serial_no: false,	// true if produced item needs a serial number.
+		serial_no_remaining: 0  	// when producing with S/N, loop this many more times
 	}
 	frappe.bnovate.work_order_execution.state = state;
 	window.state = state;
@@ -104,7 +108,11 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 			item_content.innerHTML = frappe.render_template('items_write', {
 				doc: state.ste_doc,
 			});
-			page.set_primary_action('Submit', validate);
+			if (state.serial_no_remaining > 0) {
+				page.set_primary_action('Next', validate);
+			} else {
+				page.set_primary_action('Submit', validate);
+			}
 			attach_validator();
 		}
 
@@ -125,6 +133,8 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 		state.view = read; 				// reset view
 		state.work_order_id = wo_id;
 		state.ste_doc = null;
+		state.produce_serial_no = false;
+		state.serial_no_remaining = 0;
 
 		// with_doctype loads default for the doctype, populates listview_settings,
 		// giving us status indicators
@@ -135,6 +145,10 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 		state.work_order_doc = await frappe.model.with_doc('Work Order', wo_id);
 		state.docinfo = frappe.model.docinfo;
 		state.remaining_qty = state.work_order_doc.qty - state.work_order_doc.produced_qty;
+
+		// should we switch to serialized behaviour?
+		await fetch_item_details([state.work_order_doc.production_item]);
+		state.produce_serial_no = locals["Item"][state.work_order_doc.production_item].has_serial_no;
 
 		// BOMs can't change after submit, no need to clear cache
 		state.bom_doc = await frappe.model.with_doc('BOM', state.work_order_doc.bom_no);
@@ -150,10 +164,23 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 	// LOGIC
 	////////////////////////////
 	async function finish() {
-		// Prompts user for finished qty. Generates stock entry content without submitting to DB,
-		// switched view from 'read' to 'write', to allow adjusting qties.
+		// Prompts user for finished qty.
 		page.clear_primary_action();
+
 		let qty = await prompt_qty(state.remaining_qty);
+
+		if (qty == 0) {
+			return;
+		}
+		if (state.produce_serial_no) {
+			state.serial_no_remaining = qty - 1;
+			return await build_ste(1)
+		}
+		return await build_ste(qty);
+	}
+
+	async function build_ste(qty) {
+		// Build stock entry and switch items table from read to write view to allow adjusting qties
 		let ste = await frappe.xcall('erpnext.manufacturing.doctype.work_order.work_order.make_stock_entry', {
 			'work_order_id': state.work_order_id,
 			'purpose': 'Manufacture',
@@ -180,15 +207,16 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 	function validate_inputs() {
 		// Enable Submit button only if all required fields have values.
 		const required_inputs = [...document.querySelectorAll("[data-required]")];
+		let valid = true;
 		for (let input of required_inputs) {
 			if (!input.value) {
 				input.classList.add('required')
-				return false;
+				valid = false;
 			} else {
 				input.classList.remove('required')
 			}
 		}
-		return true;
+		return valid;
 	}
 
 	function validate() {
@@ -207,7 +235,6 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 
 		submit();
 	}
-	window.validate = validate;
 
 	async function submit() {
 		// Submits STE with adjusted qties to db.
@@ -225,10 +252,21 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 			.map(([idx, batch_no]) => {
 				state.ste_doc.items.find(i => i.idx == idx).batch_no = batch_no;
 			}); // BTW, this also modifies the same object pointed to from production_item_entry.
+		// And for serial no
+		[...document.querySelectorAll("input.serial")]
+			.map(el => [el.dataset.idx, el.value || 0])
+			.map(([idx, serial_no]) => {
+				state.ste_doc.items.find(i => i.idx == idx).serial_no = serial_no;
+			});
 
-		// Create target batch if it doesn't exist
-		await create_batch_if_undefined(state.work_order_doc.production_item,
-			state.ste_doc.production_item_entry.batch_no);
+		if (state.ste_doc.production_item_entry.has_batch_no) {
+			// Create target batch if it doesn't exist
+			await create_batch_if_undefined(state.work_order_doc.production_item,
+				state.ste_doc.production_item_entry.batch_no);
+		}
+
+		handle_scrap(state.ste_doc, state.work_order_doc.production_item);
+		handle_fills(state.ste_doc, state.work_order_doc.production_item);
 
 		// Submit, post comment. Submitted doc now has a docname.
 		let submitted_doc = await frappe.db.insert(state.ste_doc);
@@ -239,9 +277,49 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 		}
 		frappe.show_alert(`<a href="#Form/Stock Entry/${submitted_doc.name}">${submitted_doc.name} created</a>`)
 
+		if (state.serial_no_remaining > 0) {
+			state.serial_no_remaining -= 1;
+			return await build_ste(state.serial_no_remaining);
+		}
 		// Reload, flash new produced qty
 		await state.load_work_order(state.work_order_id)
 		document.getElementById("produced-qty").classList.add("pulse-info");
+	}
+
+	function handle_scrap(ste_doc, bom_item) {
+		// Cleans up exceptions related to scrap items.
+		// Finds scrap items with matching SNs in 'input' items and copies SN over
+
+		let scrap_items = ste_doc.items.filter(it => !it.s_warehouse && it.item_code !== bom_item);
+		let input_items = ste_doc.items.filter(it => !!it.s_warehouse);
+
+		for (let scrap_item of scrap_items) {
+			scrap_item.serial_no = input_items
+				.filter(it => it.item_code == scrap_item.item_code)
+				.map(it => it.serial_no)
+				.join("\n");
+		}
+	}
+
+	function handle_fills(ste_doc, bom_item) {
+		// Handles special workflows related to fills refills
+		// - builds "fill associations" table.
+		// Assumes all serial numbers are filled in ste_doc.items.
+
+		if (!bom_item.startsWith("FIL")) {
+			return;
+		}
+
+		let enc_sn = ste_doc.items.find(it => !it.s_warehouse && it.item_code == "100146")?.serial_no;
+		let fill_sn = ste_doc.items.find(it => !it.s_warehouse && it.item_code == bom_item)?.serial_no;
+
+		if (enc_sn && fill_sn) {
+			ste_doc.fill_associations = [{
+				enclosure_serial: enc_sn,
+				fill_serial: fill_sn,
+			}];
+		}
+
 	}
 
 	async function prompt_qty(default_qty) {
@@ -269,6 +347,9 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 			d.show();
 		})
 	}
+
+	// SERVER CALLS
+	////////////////////////////
 
 	async function post_comment(doctype, docname, comment) {
 		await frappe.call({
@@ -311,7 +392,7 @@ frappe.pages['work-order-execution'].on_page_load = function (wrapper) {
 	////////////////////////////
 
 	function attach_validator() {
-		[...document.querySelectorAll("[data-required")]
+		[...document.querySelectorAll("[data-required]")]
 			.map(el => el.addEventListener("change", validate_inputs))
 	}
 
