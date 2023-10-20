dc95bcd29995f0b45025c31e5448db05be965df2
Douglas Watson <douglas.watson@bnovate.com>
2023-10-20 22:42:56 +0200

    Portal: allow social login with different hostname

--- bnovate/www/login.py
+++ bnovate/www/login.py
@@ -1,29 +1,24 @@
-""" Added to login by hooks.py """
-
 import urllib
 import frappe
 import frappe.utils
 
 
 def get_context(context):
-    # Fix Redirect URI
-    # Social login redirect URI contains hostname from site_config. Replace by current session hostname
+    """ Added to login context by hooks.py """
 
-    import pprint
-    pprint.pprint(context)
+    # Fix Redirect URI
+    # Social login redirect URI contains hostname from site_config. This prevents serving the 
+    # same site from several domains.
+    # Replace by it by current session hostname instead.
 
     if context.provider_logins:
         config_hostname = frappe.utils.get_url()
         session_hostname = frappe.utils.get_host_name_from_request()
 
         for provider in context.provider_logins:
-            print(provider)
-            print(urllib.parse.quote(config_hostname, ""))
-            print(urllib.parse.quote(session_hostname, ""))
-
-            # TODO: replace config hostname by session hostname in provider.auth_url
-
-
-        pprint.pprint(context.provider_logins)
+            provider['auth_url'] = provider['auth_url'].replace(
+                urllib.parse.quote(config_hostname, safe=''), 
+                urllib.parse.quote(session_hostname, safe=''),
+            )
 
     return context
