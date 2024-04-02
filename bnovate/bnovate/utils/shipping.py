97c4b62852791485edc4462b2d81f2716fa4557e
Douglas Watson <douglas.watson@bnovate.com>
2024-04-02 18:03:26 +0200

    DHL: request domestic pickup from Shipment

--- bnovate/bnovate/utils/shipping.py
+++ bnovate/bnovate/utils/shipping.py
@@ -9,22 +9,36 @@
 
 import frappe
 
+import datetime
+
 from json import JSONDecodeError
 
 from frappe import _
 from requests import request
+from requests.exceptions import HTTPError
 
 READ, WRITE = "read", "write"
+PICKUP, DELIVERY = "pickup", "delivery"
+
 
 class DHLException(Exception):
     pass
+
+
+class ProductNotFound(DHLException):
+    """ Request product was not offered in API response """
+
+class AddressError(DHLException):
+    """ Error while validating postal adress """
 
 #######################
 # HELPERS
 #######################
 
+
 def _auth(ptype=WRITE):
     return frappe.has_permission("Shipment", ptype, throw=True)
+
 
 def _get_settings():
     return frappe.get_single("bNovate Settings")
@@ -33,69 +47,307 @@ def _get_settings():
 # BASE API CONNECTIONS
 #######################
 
+
 def dhl_request(path, method='GET', params=None, body=None, settings=None, auth=True):
-    """ Make request to DHL API """
+    """ Make request to DHL API.
+     
+    Raises a DHLException if request fails
+    """
     if auth:
         _auth(READ if method == 'GET' else WRITE)
     if settings is None:
         settings = _get_settings()
     resp = request(
-        method, 
+        method,
         "https://express.api.dhl.com/mydhlapi/test" + path,
         params=params,
         json=body,
         auth=(settings.dhl_api_key, settings.dhl_api_secret),
-    ).json()
+    )
 
-    return resp
+    try:
+        resp.raise_for_status()
+    except HTTPError as e:
+        print(str(e))
+
+        try:
+            data = frappe._dict(resp.json())
+            if data.title:
+                print(data)
+                raise DHLException(
+                    "<b>{0}</b><br>{1}<br>{2}<br>{3}".format(data.title, data.message, data.detail, data.additionalDetails))
+        except TypeError:
+            # We can't convert body to JSON, raise generic error
+            pass
+
+        raise DHLException(e)
+
+    data = frappe._dict(resp.json())
+
+    return data
+
+
+def build_address(address_line1, address_line2, city, pincode, country):
+    """ Return dict of address formatted for DHL API """
+
+    country_doc = frappe.get_doc("Country", country)
+
+    address = {
+                "addressLine1": address_line1,
+                "addressLine2": address_line2,
+                "cityName": city,
+                "postalCode": pincode,
+                "countryCode": country_doc.code.upper(),
+    }
+    return frappe._dict({k: v for k, v in address.items() if v})
 
 
 #######################
 # WRAPPERS
 #######################
 
+
+def validate_address(address, address_type):
+    """ Validates if DHL Express has got pickup/delivery capabilities at origin/destination
+
+    address_type must be either 'pickup' or 'delivery'
+
+    Returns GMT Timezone offset for the destination (str)
+    """
+
+    params = {
+        "countryCode": address.countryCode,
+        "postalCode": address.postalCode,
+        "type": address_type,
+    }
+
+    resp = dhl_request(
+        "/address-validate",
+        params=params
+    )
+
+    print(resp)
+
+    try:
+        return resp['address'][0]['serviceArea']['GMTOffset']
+    except (KeyError, IndexError, TypeError) as e:
+        raise AddressError("Could not read Timezone from address")
+
+
+
 @frappe.whitelist()
 def get_price(shipment_docname):
+    """ Return price quote for a Shipment Doc. 
+
+    Includes pickup and delivery date estimates.
+    """
+
     settings = _get_settings()
     doc = frappe.get_doc("Shipment", shipment_docname)
 
+    product_code = "P"
+    local_product_code = "S"
+
+    if doc.pickup_country == "Switzerland" and doc.delivery_country == "Switzerland":
+        product_code = "N"
+        local_product_code = "N"
+
+        if len(doc.shipment_parcel) > 1:
+            raise DHLException("Domestic shipments only allow one parcel.")
+
+    # FIXME? Not specifying timezone, hopefully will use TZ from pickup address
+    pickup_datetime = datetime.datetime.combine(
+        doc.pickup_date, datetime.time()) + doc.pickup_from
+
     body = {
-        "productCode": "N",
-        "localProductCode": "N",
+        "productsAndServices": [{
+            "productCode": product_code,  # Domestic standard
+            "localProductCode": local_product_code,
+        }],
         "customerDetails": {
-            "shipperDetails": {
-                "countryCode": "CH",
-                "cityName": "Lausanne",
-                "postalCode": "1015"
-            },
-            "receiverDetails": {
-        "countryCode": "CH",
-        "cityName": "Zurich",
-                "postalCode": "5000"
-            }
+            "shipperDetails": build_address(
+                doc.pickup_address_line1,
+                doc.pickup_address_line2,
+                doc.pickup_city,
+                doc.pickup_pincode,
+                doc.pickup_country,
+            ),
+            "receiverDetails": build_address(
+                doc.delivery_address_line1,
+                doc.delivery_address_line2,
+                doc.delivery_city,
+                doc.delivery_pincode,
+                doc.delivery_country,
+            ),
         },
         "accounts": [{
             "typeCode": "shipper",
             "number": settings.dhl_export_account,
         }],
         "packages": [{
-            "weight": 5,
+            "weight": p.weight,
             "dimensions": {
-                "length": 15,
-                "width": 10,
-                "height": 5
+                "length": p.length,
+                "width": p.width,
+                "height": p.height,
             }
-        }],
-        "plannedShippingDateAndTime": "2024-03-08T17:00:31GMT+01:00",
-        "isCustomsDeclarable": False,
+        } for p in doc.shipment_parcel],
+        "plannedShippingDateAndTime": pickup_datetime.isoformat(),
+        "isCustomsDeclarable": True,
         "unitOfMeasurement": "metric"
     }
 
     resp = dhl_request("/rates", 'POST', body=body, settings=settings)
 
-    print("\n\n\n--------------------------------------------\n\n\n")
-    print("\n\n\n--------------------------------------------\n\n\n")
-    print(resp)
-    print("\n\n\n--------------------------------------------\n\n\n")
-    
+    # Find matching product
+    quote = next(
+        (p for p in resp.products if p['productCode'] == product_code), None)
+    if quote is None:
+        raise ProductNotFound()
+
+    return quote
+
+
+@frappe.whitelist()
+def create_shipment(shipment_docname):
+    """ Create Shipment, receive shipping label and tracking number """
+
+    settings = _get_settings()
+    doc = frappe.get_doc("Shipment", shipment_docname)
+
+    product_code = "P"
+    local_product_code = "S"
+
+    if doc.pickup_country == "Switzerland" and doc.delivery_country == "Switzerland":
+        product_code = "N"
+        local_product_code = "N"
+
+        if len(doc.shipment_parcel) > 1:
+            raise DHLException("Domestic shipments only allow one parcel.")
+
+    # FIXME? Not specifying timezone, hopefully will use TZ from pickup address
+    pickup_datetime = datetime.datetime.combine(
+        doc.pickup_date, datetime.time()) + doc.pickup_from
+
+    pickup_address = build_address(
+        doc.pickup_address_line1,
+        doc.pickup_address_line2,
+        doc.pickup_city,
+        doc.pickup_pincode,
+        doc.pickup_country
+    )
+    delivery_address = build_address(
+        doc.delivery_address_line1,
+        doc.delivery_address_line2,
+        doc.delivery_city,
+        doc.delivery_pincode,
+        doc.delivery_country,
+    )
+
+    # Might be off +- 1 hour if request and delivery straddle DST change, shouldn't be an issue
+    pickup_gmt_offset = validate_address(pickup_address, PICKUP)
+    validate_address(delivery_address, DELIVERY)
+
+    body = {
+        "plannedShippingDateAndTime": "{0} GMT{1}".format(pickup_datetime.isoformat(), pickup_gmt_offset),
+        "pickup": {
+            "isRequested": True,
+        },
+        "productCode": product_code,
+        "localProductCode": local_product_code,
+        "accounts": [
+            {
+                "typeCode": "shipper",
+                "number": settings.dhl_export_account,
+            }
+        ],
+        "outputImageProperties": {
+            "encodingFormat": "pdf",
+            "imageOptions": [
+                {
+                    "typeCode": "label",
+                    "templateName": "ECOM26_84_001"
+                },
+            ]
+        },
+        "customerDetails": {
+            "shipperDetails": {
+                "postalAddress": pickup_address,
+                "contactInformation": {
+                    "phone": "+41 79 012 34 56",
+                    "companyName": "Shipper AG",
+                    "fullName": "Hans Shipper",
+                                "email": "Hans.Shipper@shipper.sh"
+                }
+            },
+            "receiverDetails": {
+                "postalAddress": delivery_address,
+                "contactInformation": {
+                    "phone": "+41 79 456 78 90",
+                    "companyName": "Receiver AG",
+                    "fullName": "EXAMPLE DO NOT SHIP",
+                                "email": "Michael.receiver@receiver.ch"
+                }
+            }
+        },
+        "content": {
+            "packages": [{
+                "weight": p.weight,
+                "dimensions": {
+                    "length": p.length,
+                    "width": p.width,
+                    "height": p.height,
+                },
+                "customerReferences": [
+                    {
+                        "value": "TODO DN-12335-Test2",
+                        "typeCode": "CU"
+                    },
+                    {
+                        "value": "TODO CUSTOMER PO NUMBER",
+                        "typeCode": "AAO"
+                    }
+                ]
+
+            } for p in doc.shipment_parcel],
+            "isCustomsDeclarable": False,
+            "description": doc.description_of_content,
+            "incoterm": doc.incoterm,
+            "unitOfMeasurement": "metric"
+        },
+        "shipmentNotification": [
+            {
+                "typeCode": "email",
+                "receiverId": "douglas.watson@bnovate.com",
+                "languageCode": "eng"
+            }
+        ],
+        "valueAddedServices": [
+            {
+                "serviceCode": "PT"
+            }
+        ]
+    }
+
+    resp = dhl_request("/shipments", 'POST', body=body, settings=settings)
+
+
+    doc.db_set("carrier", "DHL")
+    doc.db_set("awb_number", resp['shipmentTrackingNumber'])
+    doc.db_set("tracking_url", resp['trackingUrl'])
+    if 'cancelPickupUrl' in resp:
+        doc.db_set("cancel_pickup_url", resp['cancelPickupUrl'])
+    doc.db_set("status", "Booked")
+    # TODO: track piece by piece?
+
+    for i, dhl_doc in enumerate(resp['documents']):
+        frappe.utils.file_manager.save_file(
+            "{0}-{1}-{2}.{3}".format(doc.name, dhl_doc['typeCode'], i, dhl_doc['imageFormat'].lower()),
+            dhl_doc['content'],
+            doc.doctype,
+            doc.name,
+            decode=True,
+            is_private=1,
+        )
+
     return resp
--- bnovate/public/js/doctype_includes/shipment.js
+++ bnovate/public/js/doctype_includes/shipment.js
@@ -10,13 +10,53 @@
 frappe.ui.form.on("Shipment", {
 
     refresh(frm) {
-        if (cur_frm.doc.docstatus == 1) {
-            frm.add_custom_button(__("Get Price"), () => get_price(frm));
+        if (cur_frm.doc.docstatus <= 1) {
+            frm.add_custom_button(__("Get Estimate"), () => get_price(frm));
+
+            // TODO: only create shipment if status is Submitted, not booked.
+            frm.add_custom_button(__("Create Shipment"), () => create_shipment(frm));
+        }
+
+
+        // CLICKABLE URLS
+
+        if (frm.doc.tracking_url) {
+            $(frm.fields_dict.tracking_url_html.wrapper).html(
+                `<div class="form-group">
+                    <div class="clearfix"><label class="control-label" style="padding-right: 0px;">${__("Tracking URL")}</label></div>
+                    <div class="control-value"><a href="${frm.doc.tracking_url}">${frm.doc.tracking_url}</a></div>
+                </div>`
+            );
+        } else {
+            $(frm.fields_dict.tracking_url_html.wrapper).html("");
+        }
+
+        if (frm.doc.cancel_pickup_url) {
+            $(frm.fields_dict.cancel_pickup_url_html.wrapper).html(
+                `<div class="form-group">
+                    <div class="clearfix"><label class="control-label" style="padding-right: 0px;">${__("Cancel pickup URL")}</label></div>
+                    <div class="control-value"><a href="${frm.doc.cancel_pickup_url}">${frm.doc.cancel_pickup_url}</a></div>
+                </div>`
+            );
+        } else {
+            $(frm.fields_dict.cancel_pickup_url_html.wrapper).html("");
         }
     },
 })
 
+function check_dirty(frm) {
+    if (frm.is_dirty()) {
+        frappe.msgprint('Please save document first');
+        return true;
+    }
+    return false
+}
+
 async function get_price(frm) {
+
+    if (check_dirty(frm))
+        return;
+
     const resp = await frappe.call({
         method: "bnovate.bnovate.utils.shipping.get_price",
         args: {
@@ -25,5 +65,28 @@ async function get_price(frm) {
     });
 
     console.log(resp.message);
+    const estimatedPickup = frappe.datetime.get_datetime_as_string(resp.message.pickupCapabilities?.localCutoffDateAndTime);
+    const estimatedDelivery = frappe.datetime.str_to_user(resp.message.deliveryCapabilities?.estimatedDeliveryDateAndTime);
+    const priceEstimate = resp.message.totalPrice?.[0].price;
+    const currency = resp.message.totalPrice?.[0].priceCurrency;
+
+    frappe.msgprint(`<b>Pickup until</b>: ${estimatedPickup}<br><b>Delivery estimated on</b>: ${estimatedDelivery}<br><b>Cost estimate</b>: ${priceEstimate} ${currency}`);
+    return resp.message;
+}
+
+async function create_shipment(frm) {
+
+    if (check_dirty(frm))
+        return;
+
+    const resp = await frappe.call({
+        method: "bnovate.bnovate.utils.shipping.create_shipment",
+        args: {
+            shipment_docname: frm.doc.name,
+        }
+    });
+
+    console.log(resp.message);
+    frm.reload_doc();
     return resp.message;
 }
