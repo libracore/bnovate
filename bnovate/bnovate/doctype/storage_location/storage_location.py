120194f87e7e644091a148fcb262a9e7bc3f647f
Douglas Watson <douglas.watson@bnovate.com>
2025-07-08 16:02:01 +0200

    Storage location: optionally check that SN exist

--- bnovate/bnovate/doctype/storage_location/storage_location.py
+++ bnovate/bnovate/doctype/storage_location/storage_location.py
@@ -69,6 +69,10 @@ def store_serial_no(location_name, serial_no, key=None):
 		frappe.throw("Serial No {} is already stored in {}".format(serial_no, location.title))
 
 	location = frappe.get_doc("Storage Location", location_name)
+	if location.ensure_valid_serial_no:
+		if not frappe.db.exists("Serial No", serial_no):
+			frappe.throw("Serial No {} does not exist.".format(serial_no))	
+
 	if key and location.key != key:
 		frappe.throw("Key does not match.")
 
